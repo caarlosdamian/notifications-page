645ac901ceba32e4e4849119bfd4437e6fa706d2
caarlosdamian <dapecarlos@gmail.com>
2022-12-28 23:49:38 -0600

    feat(notification): added some functionality

--- src/components/notificationItem/NotificationItem.tsx
+++ src/components/notificationItem/NotificationItem.tsx
@@ -4,25 +4,36 @@ import "./NotificationItem.scss";
 
 export const NotificationItem = (item: itemInterface) => {
   return (
-    <section className={`notification-wrapper ${item.read === false && 'unread'}`}>
+    <section
+      className={`notification-wrapper ${item.read === false && "unread"}`}
+    >
       <div className="notification-left-container">
         <img className="notification-img" src={item.avatar} alt="" />
       </div>
       <div className="notification-middle-container">
-        <div className={`notification-middle-text-container ${item.read === false && 'unread' }`}>
-          {/* <p className="text-labels"> */}
-            <strong className="text-name">{item.name}</strong>
-            <span className="text-action"> {item.action}</span>
-            <span className="hightlight"> {item.hightlight} </span>
-            {/* {item.read === false && <div className="circle"></div>} */}
-          {/* </p> */}
+        <div
+          className={`notification-middle-text-container ${
+            item.read === false && "unread"
+          }`}
+        >
+          <strong className="text-name">{item.name}</strong>
+          <span className="text-action"> {item.action}</span>
+          <span className="hightlight"> {item.hightlight} </span>
         </div>
         <div className="notification-middle-time-container">
           <span className="time-label">{item.time}</span>
         </div>
-        <div className="notification-middle-message"></div>
+        {item.ismessage && (
+          <div className="notification-middle-message">
+            <span className="notification-message">{item.message}</span>
+          </div>
+        )}
       </div>
-      <div className="notification-right-container"></div>
+      {item.secondImg !== "" && (
+        <div className="notification-right-container">
+          {<img src={item.secondImg} alt="" className="second-img" />}
+        </div>
+      )}
     </section>
   );
 };
