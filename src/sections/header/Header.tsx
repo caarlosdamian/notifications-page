d170685b7b2bb65071cb002aa7146dd78f743249
caarlosdamian <dapecarlos@gmail.com>
2022-12-28 19:46:24 -0600

    feat(header): added header component

--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./Header.scss";
 
 export const Header = () => {
   return (
-    <header>Header</header>
-  )
-}
+    <header className="header-container">
+      <section className="left-container">
+        <p className="left-label">Notifications</p>
+        <p className="left-number">5</p>
+      </section>
+      <section className="right-container">
+        <p className="right-label">Mark all as read</p>
+      </section>
+    </header>
+  );
+};
